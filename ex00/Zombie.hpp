63d144ae36d552819dd32b5f4608f397512fd7da
Antoine Bouclier <abouclie@z3r6p6.42lyon.fr>
2025-10-02 14:42:19 +0200

    Adding Makefile to all the exo

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 12:36:16 by abouclie          #+#    #+#             */
+/*   Updated: 2025/10/02 14:39:52 by abouclie         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class	Zombie
+{
+	private:
+		std::string	_name;
+	public:
+		void	announce(void);
+		Zombie*	newZombie(std::string name);
+		void	randomChump(std::string name);
+};
+
+#endif
