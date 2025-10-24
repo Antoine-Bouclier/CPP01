246dfdd6248f2d056d2a32c60e6aff41f47e184f
Antoine Bouclier <abouclie@z3r8p1.42lyon.fr>
2025-10-24 14:06:28 +0200

    ex03: starting HumanA and HumanB class

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -1,6 +1,15 @@
 #ifndef HUMAN_A_HPP
 #define HUMAN_A_HPP
 
+#include "iostream"
 
+class	HumanA
+{
+	private:
+		std::string	_weapon_type;
+	public:
+		HumanA();
+		~HumanA();
+};
 
 #endif
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -1,6 +1,15 @@
 #ifndef HUMAN_B_HPP
 #define HUMAN_B_HPP
 
+#include "iostream"
 
+class	HumanB
+{
+	private:
+		std::string	_weapon_type;
+	public:
+		HumanB();
+		~HumanB();
+};
 
 #endif
