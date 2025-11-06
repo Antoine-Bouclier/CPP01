3abc80472c47b54b0afe6e891cd372bb2de47a39
Antoine Bouclier <abouclie@z4r6p4.42lyon.fr>
2025-11-06 14:21:55 +0100

    fix: Ex02

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:13:20 by abouclie          #+#    #+#             */
-/*   Updated: 2025/10/27 08:52:05 by abouclie         ###   ########lyon.fr   */
+/*   Updated: 2025/11/06 14:20:49 by abouclie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	main(void)
 
 	string = "HI THIS IS BRAIN";
 	std::cout << "Adress of the string:\t\t" << &string << std::endl;
-	std::cout << "Adress of the stringPtr:\t" << &stringPtr << std::endl;
+	std::cout << "Adress of the stringPtr:\t" << stringPtr << std::endl;
 	std::cout << "Adress of the stringRef:\t" << &stringRef << std::endl;
 	std::cout << std::endl;
 	std::cout << "Value of the string:\t\t" << string << std::endl;
-	std::cout << "Value of the stringPtr:\t\t" << stringPtr << std::endl;
+	std::cout << "Value of the stringPtr:\t\t" << *stringPtr << std::endl;
 	std::cout << "Value of the stringRef:\t\t" << stringRef << std::endl;
 	return (0);
 }
