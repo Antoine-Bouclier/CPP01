6bd8e86a19f799e8a1dd0734a1278cd85e01e720
Antoine Bouclier <abouclie@z4r6p4.42lyon.fr>
2025-11-06 14:20:20 +0100

    fix: Ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 10:14:10 by abouclie          #+#    #+#             */
-/*   Updated: 2025/10/29 10:56:07 by abouclie         ###   ########lyon.fr   */
+/*   Updated: 2025/11/06 14:19:01 by abouclie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@ int	main()
 	int		zombieAmount = 10;
 
 	horde = zombieHorde(zombieAmount, "groups");
-	if (horde == NULL)
-		return (1);
 	for (int i = 0; i < zombieAmount; ++i)
 		horde[i].announce();
 	delete[] horde;
-	horde = zombieHorde(0, "Failed");
+	horde = zombieHorde(-1, "Failed");
 	horde = zombieHorde(1, "solo");
 	horde[0].announce();
 	delete[] horde;
--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 10:13:42 by abouclie          #+#    #+#             */
-/*   Updated: 2025/10/24 12:06:48 by abouclie         ###   ########lyon.fr   */
+/*   Updated: 2025/11/06 14:16:54 by abouclie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 Zombie* zombieHorde( int N, std::string name )
 {
 	if (N < 1)
-	std::cout << "A horde must contain at least 1 zombie" << std::endl;
-	Zombie	*horde = new Zombie[N];
-	if (!horde)
+	{
+		std::cout << "A horde must contain at least 1 zombie" << std::endl;
 		return (NULL);
+	}
+	Zombie	*horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		horde[i].set_name(name);
 	return (horde);
