a04ed34c1510b4bdeb3e6d93332c28f276dd4ce2
Antoine Bouclier <abouclie@z4r6p4.42lyon.fr>
2025-11-06 09:45:08 +0100

    Ex04: Done.

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,24 +6,50 @@
 /*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 18:54:13 by abouclie          #+#    #+#             */
-/*   Updated: 2025/11/04 19:10:44 by abouclie         ###   ########lyon.fr   */
+/*   Updated: 2025/11/06 09:44:32 by abouclie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-// #include <string>
-// ofstream - docs a regarder
+#include <sstream>
 
-int	main()
+int	main(int argc, char **argv)
 {
-	std::ifstream	inputFile("files/abc.txt");
-	std::string		line;
-
-	while (getline(inputFile, line))
+	std::ifstream		ifs;
+	std::ofstream		ofs;
+	std::stringstream	buffer;
+	std::string			filename, content, s1, s2;
+	std::size_t			pos;
+	
+	if (argc != 4)
 	{
-		std::cout << line << std::endl;
+		std::cerr << "Usage: ./replace <filename> <s1> <s2>\n";
+		return (1);
 	}
-	inputFile.close();
+	filename = argv[1], s1 = argv[2], s2 = argv[3];
+	ifs.open(argv[1]);
+	if (!ifs.is_open())
+	{
+		std::cerr << "Error opening file\n";
+		return (1);
+	}
+	buffer << ifs.rdbuf();
+	ifs.close();
+	content = buffer.str();
+	while ((pos = content.find(s1)) != std::string::npos)
+	{
+		content.erase(pos, s1.length());
+		content.insert(pos, s2);
+	}
+	filename += ".replace";
+	ofs.open(filename.c_str());
+	if (!ofs.is_open())
+	{
+		std::cerr << "Error opening file\n";
+		return (1);
+	}
+	ofs.write(content.c_str(), content.length());
+	ofs.close();
 	return (0);
 }
